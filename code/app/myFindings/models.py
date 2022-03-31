5623da64e0c7a34c1e865eaf3a6b62b9272ebcc6
Joaquín Alejandro España Sánchez <joaquinalejandroespana@gmail.com>
2022-03-31 20:16:10 +0200

    Removes the required in the forms and in the database, change some model fields #15, #18, close #15, close #20

--- code/app/myFindings/models.py
+++ code/app/myFindings/models.py
@@ -2,14 +2,15 @@ from django.db import models
 
 FASE_CHOICES = [
     (('A: época contemporánea'), (
-            ('A1','A1: 1800/1900'),
-            ('A2','A2: 1900/2000'),
-            ('A3','A3: época actual'),
+            ('A1','A1: Siglo XVIII'),
+            ('A2','A2: Siglo XIX'),
+            ('A3','A3: Siglo XX'),
         )
     ),
     (('B: época moderna'),(
-            ('B1','B1: fase morisca'),
-            ('B2','B2: fase cristiana'),
+            ('B1','B1: Siglo XV'),
+            ('B2','B2: Siglo XVI'),
+            ('B3','B3: Siglo XVII'),
         )
     ),
     (('C: época medieval'), (
@@ -28,42 +29,45 @@ FASE_CHOICES = [
         )
     ),
     (('E: época ibérica'), (
-            ('E1','E1: Protoibérico'),
+            ('E1','E1: Protoibérico-orientalizante'),
             ('E2','E2: Ibérico antiguo'),
             ('E3','E3: Ibérico pleno'),
             ('E4','E4: Ibérico tardío o final'),
         )
     ),
     (('F: Edad del Bronce'), (
-            ('F1','F1: Bronce Final'),
-            ('F2','F2: Bronce argárico'),
-            ('F3','F3: Bronce antiguo'),
+            ('F1','F1: Bronce Antiguo'),
+            ('F2','F2: Bronce Pleno'),
+            ('F3','F3: Bronce Tardío'),
+            ('F4','F4: Bronce Final'),
         )
     ),
 ]
 
 PERIODO_CHOICES = [
-    ('Contemporánea: 1800/1900','Contemporánea: 1800/1900'),
-    ('Contemporánea: 1900/2000','Contemporánea: 1900/2000'),
-    ('Contemporánea: actualidad','Contemporánea: actualidad'),
-    ('Moderna: morisca','Moderna: morisca'),
-    ('Moderna: cristiana','Moderna: cristiana'),
-    ('Medieval: emiral','Medieval: emiral'),
-    ('Medieval: califal','Medieval: califal'),
-    ('Medieval: taifas','Medieval: taifas'),
-    ('Medieval: almorávide/almohade','Medieval: almorávide/almohade'),
-    ('Medieval: nazarí','Medieval: nazarí'),
-    ('Romana: época republicana','Romana: época republicana'),
-    ('Romana: Alto Imperio','Romana: Alto Imperio'),
-    ('Romana: Bajo Imperio','Romana: Bajo Imperio'),
-    ('Romana: Antigüedad tardía','Romana: Antigüedad tardía'),
-    ('Ibérica: Protoibérico','Ibérica: Protoibérico'),
-    ('Ibérica: Ibérico antiguo','Ibérica: Ibérico antiguo'),
-    ('Ibérica: Ibérico pleno','Ibérica: Ibérico pleno'),
-    ('Ibérica: Ibérico tardío o final','Ibérica: Ibérico tardío o final'),
-    ('Edad del Bronce: final','Edad del Bronce: final'),
-    ('Edad del Bronce: argárico','Edad del Bronce: argárico'),
-    ('Edad del Bronce: antiguo','Edad del Bronce: antiguo'),
+    ('Siglo XVIII','Contemporánea: Siglo XVIII'),
+    ('Siglo XIX','Contemporánea: Siglo XIX'),
+    ('Siglo XX','Contemporánea: Siglo XX'),
+    ('Siglo XV','Moderna: Siglo XV'),
+    ('Siglo XVI','Moderna: Siglo XVI'),
+    ('Siglo XVII','Moderna: Siglo XVII'),
+    ('Fase emiral','Medieval: emiral'),
+    ('Fase califal','Medieval: califal'),
+    ('Fase taifas','Medieval: taifas'),
+    ('Fase invasiones almorávides y almohades','Medieval: almorávide/almohade'),
+    ('Fase nazarí','Medieval: nazarí'),
+    ('Fase republicana','Romana: época republicana'),
+    ('Fase Altoimperial','Romana: Alto Imperio'),
+    ('Fase Bajoimperial','Romana: Bajo Imperio'),
+    ('Antigüedad tardía','Romana: Antigüedad tardía'),
+    ('Protoibérico-orientalizante','Ibérica: Protoibérico'),
+    ('Ibérico antiguo','Ibérica: Ibérico antiguo'),
+    ('Ibérico pleno','Ibérica: Ibérico pleno'),
+    ('Ibérico final','Ibérica: Ibérico tardío o final'),
+    ('Bronce antiguo','Edad del Bronce: Antiguo'),
+    ('Bronce pleno','Edad del Bronce: Pleno'),
+    ('Bronce tardío','Edad del Bronce: Tardío'),
+    ('Bronce final','Edad del Bronce: Final'),
 ]
 
 
@@ -73,18 +77,18 @@ PERIODO_CHOICES = [
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 class Estancia(models.Model):
     n_estancia = models.CharField(max_length=5, unique=True)     # ES001, ES002, etc
-    n_zona = models.PositiveSmallIntegerField()
-    n_sector = models.PositiveSmallIntegerField()
-    observaciones = models.CharField(max_length=200)
-    croquis_planta = models.ImageField()
-    n_planta = models.PositiveSmallIntegerField()
-    n_seccion = models.PositiveSmallIntegerField()
-    elevacion = models.PositiveSmallIntegerField()
-    tpq = models.PositiveSmallIntegerField()
-    taq = models.PositiveSmallIntegerField()
-    fase = models.CharField(max_length=2, choices=FASE_CHOICES)
-    periodo = models.CharField(max_length=100, choices=PERIODO_CHOICES)
-    autor = models.CharField(max_length=50)
+    n_zona = models.PositiveSmallIntegerField(blank=True, null=True)
+    n_sector = models.PositiveSmallIntegerField(blank=True, null=True)
+    observaciones = models.CharField(max_length=200, blank=True, null=True)
+    croquis_planta = models.ImageField(blank=True, null=True)
+    n_planta = models.PositiveSmallIntegerField(blank=True, null=True)
+    n_seccion = models.PositiveSmallIntegerField(blank=True, null=True)
+    elevacion = models.PositiveSmallIntegerField(blank=True, null=True)
+    tpq = models.PositiveSmallIntegerField(blank=True, null=True)
+    taq = models.PositiveSmallIntegerField(blank=True, null=True)
+    fase = models.CharField(max_length=2, choices=FASE_CHOICES, blank=True, null=True)
+    periodo = models.CharField(max_length=100, choices=PERIODO_CHOICES, blank=True, null=True)
+    autor = models.CharField(max_length=50, blank=True, null=True)
 
     def __str__(self):
         return self.n_estancia
@@ -107,23 +111,23 @@ class Hecho(models.Model):
     ]
 
     # Foreign keys
-    estancia = models.ForeignKey(Estancia, on_delete=models.CASCADE)
+    estancia = models.ForeignKey(Estancia, on_delete=models.CASCADE, blank=True, null=True)
 
     # Unique key
     letra = models.CharField(max_length=2, choices=LETRA_CHOICES)
     numero = models.CharField(max_length=6)    # Number of stratigrafic unit that identifies the fact
 
-    fase = models.CharField(max_length=2, choices=FASE_CHOICES)
-    tpq = models.PositiveSmallIntegerField()
-    taq = models.PositiveSmallIntegerField()
-    definicion = models.CharField(max_length=100)
-    comentarios = models.CharField(max_length=100)
-    sector = models.PositiveSmallIntegerField()
-    zona = models.PositiveSmallIntegerField()
-    año = models.PositiveSmallIntegerField()
-    estructura = models.PositiveSmallIntegerField() 
-    croquis_plan = models.ImageField()
-    croquis_seccion = models.ImageField()
+    fase = models.CharField(max_length=2, choices=FASE_CHOICES, blank=True, null=True)
+    tpq = models.PositiveSmallIntegerField(blank=True, null=True)
+    taq = models.PositiveSmallIntegerField(blank=True, null=True)
+    definicion = models.CharField(max_length=100, blank=True, null=True)
+    comentarios = models.CharField(max_length=100, blank=True, null=True)
+    sector = models.PositiveSmallIntegerField(blank=True, null=True)
+    zona = models.PositiveSmallIntegerField(blank=True, null=True)
+    año = models.PositiveSmallIntegerField(blank=True, null=True)
+    estructura = models.PositiveSmallIntegerField(blank=True, null=True) 
+    croquis_plan = models.ImageField(blank=True, null=True)
+    croquis_seccion = models.ImageField(blank=True, null=True)
 
     class Meta:
         constraints = [
@@ -138,9 +142,9 @@ class Hecho(models.Model):
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 class Excavacion(models.Model):
     n_excavacion = models.PositiveIntegerField(unique=True, verbose_name='Número de excavación')
-    latitud = models.FloatField()
-    longitud = models.FloatField()
-    altura = models.PositiveSmallIntegerField()
+    latitud = models.FloatField(blank=True, null=True)
+    longitud = models.FloatField(blank=True, null=True)
+    altura = models.PositiveSmallIntegerField(blank=True, null=True)
 
     def __str__(self):
         return str(self.n_excavacion)
@@ -150,13 +154,13 @@ class Excavacion(models.Model):
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 class UE(models.Model):
     INTERPRETACION_CHOICES = [
-        ('Ocupacion','Ocupación'),
+        ('Ocupación','Ocupación'),
         ('Abandono','Abandono'),
-        ('Contruccion','Construcción'),
+        ('Contrucción','Construcción'),
         ('Paleosuelo','Paleosuelo'),
-        ('Reinstalacion','Reinstalación'),
-        ('Coluvion','Coluvión/Aluvión'),
-        ('Destruccion','Destrucción'),
+        ('Reinstalación','Reinstalación'),
+        ('Coluvión/Aluvión','Coluvión/Aluvión'),
+        ('Destrucción','Destrucción'),
         ('Relleno','Relleno'),
         ('Otras','Otras'),
     ]
@@ -175,32 +179,32 @@ class UE(models.Model):
     codigo = models.CharField(unique=True, max_length=6, default='000000')
     # Foreign Keys
     hecho = models.ForeignKey(Hecho, on_delete=models.CASCADE, blank=True, null=True)
-    excavacion = models.ForeignKey(Excavacion, on_delete=models.CASCADE)
+    excavacion = models.ForeignKey(Excavacion, on_delete=models.CASCADE, to_field='n_excavacion')
 
-    plano_n = models.PositiveSmallIntegerField()
-    seccion_n = models.PositiveSmallIntegerField()
-    elevacion_n = models.PositiveSmallIntegerField()
-    croquis_planta = models.ImageField()
-    croquis_seccion = models.ImageField()
-    tpq = models.PositiveSmallIntegerField()
-    taq = models.PositiveSmallIntegerField()
-    fase = models.CharField(max_length=2, choices=FASE_CHOICES)
-    periodo = models.CharField(max_length=100, choices=PERIODO_CHOICES)
-    descripcion = models.TextField(max_length=200)
-    autor = models.CharField(max_length=30)
-    año = models.PositiveSmallIntegerField()
-    interpretacion = models.CharField(max_length=13, choices=INTERPRETACION_CHOICES)    
-    sector = models.PositiveSmallIntegerField()          
-    observaciones = models.TextField(max_length=200)
-    latitud = models.FloatField()
-    longitud = models.FloatField()
+    plano_n = models.PositiveSmallIntegerField(blank=True, null=True)
+    seccion_n = models.PositiveSmallIntegerField(blank=True, null=True)
+    elevacion_n = models.PositiveSmallIntegerField(blank=True, null=True)
+    croquis_planta = models.ImageField(blank=True, null=True)
+    croquis_seccion = models.ImageField(blank=True, null=True)
+    tpq = models.PositiveSmallIntegerField(blank=True, null=True)
+    taq = models.PositiveSmallIntegerField(blank=True, null=True)
+    fase = models.CharField(max_length=2, choices=FASE_CHOICES, blank=True, null=True)
+    periodo = models.CharField(max_length=100, choices=PERIODO_CHOICES, blank=True, null=True)
+    descripcion = models.TextField(max_length=200, blank=True, null=True)
+    autor = models.CharField(max_length=30, blank=True, null=True)
+    año = models.PositiveSmallIntegerField(blank=True, null=True)
+    interpretacion = models.CharField(max_length=16, choices=INTERPRETACION_CHOICES, blank=True, null=True)    
+    sector = models.PositiveSmallIntegerField(blank=True, null=True)          
+    observaciones = models.TextField(max_length=200, blank=True, null=True)
+    latitud = models.FloatField(blank=True, null=True)
+    longitud = models.FloatField(blank=True, null=True)
 
 
-    cota_superior_diff = models.IntegerField()
-    cota_inferior_diff = models.IntegerField()
+    cota_superior_diff = models.SmallIntegerField(blank=True, null=True)
+    cota_inferior_diff = models.SmallIntegerField(blank=True, null=True)
 
-    pendiente_superior = models.CharField(max_length=8, choices=PENDIENTE_CHOICES)
-    pendiente_inferior = models.CharField(max_length=8, choices=PENDIENTE_CHOICES)
+    pendiente_superior = models.CharField(max_length=8, choices=PENDIENTE_CHOICES, blank=True, null=True)
+    pendiente_inferior = models.CharField(max_length=8, choices=PENDIENTE_CHOICES,  blank=True, null=True)
 
     def _get_cota_superior(self):
         return self.excavacion.altura + self.cota_superior_diff
@@ -216,17 +220,18 @@ class UE(models.Model):
         return str(self.descripcion)
 
 class Fotografia(models.Model):
-    # Foreign Keys
-    ue = models.ForeignKey(UE, on_delete=models.CASCADE)
-    estancia = models.ForeignKey(Estancia, on_delete=models.CASCADE)
-
     numero = models.PositiveIntegerField(unique=True)
-    tipo = models.CharField(max_length=50)
-    fase = models.CharField(max_length=2, choices=FASE_CHOICES)
-    vista_desde = models.CharField(max_length=50)
-    dist_focal = models.PositiveSmallIntegerField()
-    descripcion = models.TextField(max_length=200)
-    imagen = models.ImageField()
+   
+    # Foreign Keys
+    ue = models.ForeignKey(UE, on_delete=models.CASCADE, blank=True, null=True)
+    estancia = models.ForeignKey(Estancia, on_delete=models.CASCADE, blank=True, null=True)
+
+    tipo = models.CharField(max_length=50, blank=True, null=True)
+    fase = models.CharField(max_length=2, choices=FASE_CHOICES, blank=True, null=True)
+    vista_desde = models.CharField(max_length=50, blank=True, null=True)
+    dist_focal = models.PositiveSmallIntegerField(blank=True, null=True)
+    descripcion = models.TextField(max_length=200, blank=True, null=True)
+    imagen = models.ImageField(blank=True, null=True)
 
     def __str__(self):
         return self.numero
@@ -247,7 +252,7 @@ class MaterialSedimentaria(models.Model):
         ('Otros', 'Otros'),
     ]
 
-    nombre = models.CharField(max_length=20, choices=NOMBRE_CHOICES, primary_key=True)
+    nombre = models.CharField(max_length=21, choices=NOMBRE_CHOICES, primary_key=True)
 
     def __str__(self):
         return self.nombre
@@ -291,23 +296,23 @@ class UESedimentaria(UE):
         ('Mortero','Mortero'),
     ]
 
-    tipo_estructura = models.CharField(max_length=15, choices=ESTRUCTURA_CHOICES)
-    tipo_textura = models.CharField(max_length=10, choices=TEXTURA_CHOICES)
-    materiales_sedimentarios = models.ManyToManyField(MaterialSedimentaria)
+    tipo_estructura = models.CharField(max_length=15, choices=ESTRUCTURA_CHOICES, blank=True, null=True)
+    tipo_textura = models.CharField(max_length=10, choices=TEXTURA_CHOICES, blank=True, null=True)
+    materiales = models.ManyToManyField(MaterialSedimentaria, blank=True)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # UECONSTRUIDA        ~~~~~~~~
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 class UEConstruida(UE):
     TIPO_CHOICES = [
-        ('POS', 'Positiva'),
-        ('NEG', 'Negativa'),
+        ('Positiva', 'Positiva'),
+        ('Negativa', 'Negativa'),
     ]
 
-    sistema_constructivo = models.CharField(max_length=50)
-    tipo = models.CharField(max_length=3, choices=TIPO_CHOICES)
-    materiales_construidos = models.ManyToManyField(MaterialConstruida)
-    n_estructura = models.PositiveSmallIntegerField(default=0)
+    sistema_constructivo = models.CharField(max_length=50, blank=True, null=True)
+    tipo = models.CharField(max_length=8, choices=TIPO_CHOICES, blank=True, null=True)
+    materiales = models.ManyToManyField(MaterialConstruida, blank=True)
+    n_estructura = models.PositiveSmallIntegerField(default=0, blank=True, null=True)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # INCLUSION           ~~~~~~~~
@@ -345,8 +350,8 @@ class Inclusion(models.Model):
     # Foreign Keys
     uesedimentaria = models.ForeignKey(UESedimentaria, on_delete=models.CASCADE)
 
-    frecuencia = models.CharField(max_length=10, choices=FRECUENCIA_CHOICES)
-    grosor = models.CharField(max_length=10, choices=GROSOR_CHOICES)
+    frecuencia = models.CharField(max_length=10, choices=FRECUENCIA_CHOICES, blank=True, null=True)
+    grosor = models.CharField(max_length=10, choices=GROSOR_CHOICES, blank=True, null=True)
 
     class Meta:
         constraints = [
